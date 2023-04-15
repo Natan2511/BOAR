420265f88c7459defa4d568496960e58310c7b28
Natan2511 <114068925+Natan2511@users.noreply.github.com>
2023-04-16 00:06:28 +0300

    fix call and scroll

--- src/js/call.js
+++ src/js/call.js
@@ -1,6 +1,5 @@
 const modalOpenButtons = document.querySelectorAll('[call--modal__open]');
 const modal = document.querySelector('.call--modal');
-const modalCloseButton = document.querySelector('.call--modal__close');
 
 modalOpenButtons.forEach(button => {
   button.addEventListener('click', () => {
@@ -8,20 +7,31 @@ modalOpenButtons.forEach(button => {
   });
 });
 
-modalCloseButton.addEventListener('click', () => {
+function closeModal() {
   modal.style.display = 'none';
+}
+
+document.addEventListener('mousedown', event => {
+  if (!modal.contains(event.target)) {
+    closeModal();
+  }
 });
 
-// // Закрытие модального окна при клике за его границами
-// window.addEventListener('click', event => {
-//   if (event.target == modal) {
-//     modal.style.display = 'none';
-//   }
-// });
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
 
-// // Закрытие модального окна при нажатии на клавишу Esc
-// document.addEventListener('keydown', event => {
-//   if (event.key === 'Escape') {
-//     modal.style.display = 'none';
-//   }
-// });
+modal.addEventListener('touchstart', event => {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+// Закрытие модального окна при клике за его границами
+window.addEventListener('click', event => {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+});
