9bef8a99f0b9ed69333a873d9b7e634263206b65
Natan2511 <114068925+Natan2511@users.noreply.github.com>
2023-04-16 03:23:55 +0300

    complete gallery

--- src/js/swiper_gallery.js
+++ src/js/swiper_gallery.js
@@ -5,6 +5,21 @@ var swiperGallery = new Swiper('.swiperGallery', {
     el: '.swiper-pagination',
     clickable: true,
   },
+  breakpoints: {
+    // Конфигурация для разных ширин экрана
+    769: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    319: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
 });
 
 $(document).ready(function () {
